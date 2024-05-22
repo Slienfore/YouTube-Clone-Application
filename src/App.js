52f8a93c764f4f1e15a08a48e3e43ca3a7872989
Slienfore <Slienfore@163.com>
2024-05-22 19:22:34 +0800

    Building App Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+// BrowserRouter： Router 顶层组件, 使整个App具有使用路由动能
+// Routes: 用于定义路由规则, 包含所有 Route 组件, 根据路径匹配组件
+// Route: 定义单个路由规则, 指定路径与之对应的组件
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  NavBar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
+
 const App = () => {
-  return <div>Hello React</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" exact element={<Feed />}></Route>
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
