f6408e4fadd659e5d9d59a8cc18da496e1e649ec
Slienfore <Slienfore@163.com>
2024-05-23 10:05:41 +0800

    Building ChannelCard and VideoCard

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,20 @@
-const Videos = ({ videos }) => {
-  console.log(videos);
+import { Stack, Box } from "@mui/material";
+import { ChannelCard, VideoCard } from "./";
 
-  return <div>Videos</div>;
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, i) => (
+        <Box key={i}>
+          {/* video card */}
+          {item.id.videoId && <VideoCard video={item} />}
+
+          {/* channel info card */}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
